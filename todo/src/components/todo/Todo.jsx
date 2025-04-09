acfcbb70203cfc376d53a31d90e5e7b84143967d
konul02 <rmnselimli@gmail.com>
2025-04-09 13:41:51 +0400

    update

--- todo/src/components/todo/Todo.jsx
+++ todo/src/components/todo/Todo.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { useState } from "react";
+
 function Todo() {
   const [text, setText] = useState("");
   const [todos, setTodos] = useState([]);
+
   const Todo = () => {
     console.log(text);
     const todo = {
@@ -10,13 +12,21 @@ function Todo() {
     };
     setTodos((newTodo) => [...newTodo, todo]);
   };
+
   return (
     <div>
       <input onChange={(e) => setText(e.target.value)} type="text" />
       <button onClick={Todo}>Click</button>
       <ul>
         {todos.map((todo, index) => {
-          return <li key={index}>{todo.text}</li>;
+          return (
+            <li key={index}>
+              {todo.text}
+              <button onClick={() => setTodos(todos.filter((_, i) => i !== index))}>
+                Delete
+              </button>
+            </li>
+          );
         })}
       </ul>
     </div>
